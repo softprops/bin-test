4dcc20b20f66b05665756e54d22afad419a42966
softprops <d.tangren@gmail.com>
2017-01-16 12:38:22 -0500

    smaller set of targets, https call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate hyper;
+
+use std::io::Read;
+
 fn main() {
-    println!("sup")
+    println!("sup google");
+    std::io::copy(&mut hyper::Client::new().get("https://www.google.com/").send().unwrap(), &mut std::io::stdout());
 }
