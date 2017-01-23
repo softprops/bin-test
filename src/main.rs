ba6394c06d4ba5df6bbaf5b124ab525540ae93ee
softprops <d.tangren@gmail.com>
2017-01-22 20:29:32 -0500

    latest hyper with rustls instead of openssl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate hyper;
+extern crate hyper_rustls;
 
-use std::io::Read;
+use hyper::net::HttpsConnector;
+use hyper_rustls::TlsClient;
 
 fn main() {
     println!("sup google");
-    std::io::copy(&mut hyper::Client::new().get("https://www.google.com/").send().unwrap(), &mut std::io::stdout());
+    let _ = std::io::copy(&mut hyper::Client::with_connector(HttpsConnector::new(TlsClient::new())).get("https://www.google.com/").send().unwrap(), &mut std::io::stdout());
 }
