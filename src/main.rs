6f162c3affff503b26d20b8e8877805a6dffb4a5
softprops <d.tangren@gmail.com>
2017-01-22 20:47:07 -0500

    try native tls

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 extern crate hyper;
-extern crate hyper_rustls;
+
+extern crate hyper_native_tls;
+//extern crate hyper_rustls;
 
 use hyper::net::HttpsConnector;
-use hyper_rustls::TlsClient;
+use hyper_native_tls::NativeTlsClient;
+//use hyper_rustls::TlsClient;
 
 fn main() {
     println!("sup google");
-    let _ = std::io::copy(&mut hyper::Client::with_connector(HttpsConnector::new(TlsClient::new())).get("https://www.google.com/").send().unwrap(), &mut std::io::stdout());
+    let _ = std::io::copy(
+        &mut hyper::Client::with_connector(
+            HttpsConnector::new(
+                NativeTlsClient::new().unwrap()
+            )
+        ).get("https://www.google.com/").send().unwrap(), &mut std::io::stdout()
+    );
 }
